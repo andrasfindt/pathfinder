a7fc220d4fc13d0d5668c04d7ebd937e970d5938
Andras Findt <anthrax.f@gmail.com>
2019-02-22 20:05:19 +0200

    Cheating by giving the thing a pile of RAM to play with. (unnecessarily high amount of RAM)

--- mazesolver-ui/run.sh
+++ mazesolver-ui/run.sh
@@ -5,4 +5,4 @@ source_home=/Users/andrasfindt/Development/zyx.andrast/genetic-algorithm-tutoria
 mvn_exec=/usr/local/Cellar/maven@3.3/3.3.9/libexec
 mvn_conf=/usr/local/Cellar/maven@3.3/3.3.9/libexec/bin/m2.conf
 mvn_classpath="/usr/local/Cellar/maven@3.3/3.3.9/libexec/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher"
-java -Dmaven.multiModuleProjectDirectory=${source_home} ${DEBUG} -Dmaven.home=${mvn_exec} -Dclassworlds.conf=${mvn_conf} -Dfile.encoding=UTF-8 -classpath ${mvn_classpath} clean compile exec:java > run.log
+java -Xmx1g -Dmaven.multiModuleProjectDirectory=${source_home} ${DEBUG} -Dmaven.home=${mvn_exec} -Dclassworlds.conf=${mvn_conf} -Dfile.encoding=UTF-8 -classpath ${mvn_classpath} clean compile exec:java > run.log
