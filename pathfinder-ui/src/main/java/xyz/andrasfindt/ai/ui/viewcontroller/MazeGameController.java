6a11ee02c77de9e86944be0b73721a01b53df983
Andras Findt <anthrax.f@gmail.com>
2019-02-23 23:16:41 +0200

    Changed barrier color. removed comments

--- pathfinder-ui/src/main/java/xyz/andrasfindt/ai/ui/viewcontroller/MazeGameController.java
+++ pathfinder-ui/src/main/java/xyz/andrasfindt/ai/ui/viewcontroller/MazeGameController.java
@@ -30,11 +30,9 @@ import java.util.List;
 public class MazeGameController implements Listener, DrawingListener {
     private static final double GREEN_HUE = Color.GREEN.getHue();
     private static final double GREEN_HUE_SCALING_FACTOR = GREEN_HUE / (Game.Setup.SCREEN_HEIGHT - Game.Setup.goal.y / 2d - 1d);
-    private static final Color OBSTACLE_COLOR = Color.FIREBRICK;
+    private static final Color OBSTACLE_COLOR = Color.PERU;
     private static final String LOG_MESSAGE_FORMAT = "%s %s gen: %d max: %s step(t/m): %d/%d max_speed: %s creeps(a/p): %d/%d m-rate: %s r-seed: %d\n";
     private static final Color DEFAULT_COLOR = Color.hsb(0d, 1d, 1d);//Color.BLUEVIOLET;
-    //    private static final double HUE_SCALE = ;
-    //    private static final double SATURATION_SCALE = 1d / (Game.Setup.SCREEN_HEIGHT - 1);
     @FXML
     public CheckBox solved;
     @FXML
@@ -132,8 +130,8 @@ public class MazeGameController implements Listener, DrawingListener {
             graphicsContext.setFill(currentColor);
         } */ else {
             double hue = getHue(position);
-            double saturation = 1d;//clamp(0d, position.y, Game.Setup.SCREEN_WIDTH - 1) * SATURATION_SCALE;
-            currentColor = Color.hsb(hue, saturation, 1.0);//DEFAULT_POPULATION;
+            double saturation = 1d;
+            currentColor = Color.hsb(hue, saturation, 1.0);
             graphicsContext.setStroke(currentColor);
             graphicsContext.setFill(currentColor);
         }
